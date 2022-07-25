0d356fa85b56c6755150a269cf0f7dce092e4234
Matheus Amorim <mtsgamorim@gmail.com>
2022-07-25 13:19:51 -0300

    now imagem from games have to be url

--- middlewares/gamesMiddlewares.js
+++ middlewares/gamesMiddlewares.js
@@ -5,7 +5,7 @@ async function validateGame(req, res, next) {
   const game = req.body;
   const gameSchema = joi.object({
     name: joi.string().required(),
-    image: joi.string().required(),
+    image: joi.string().uri().required(),
     stockTotal: joi.number().greater(0).required(),
     categoryId: joi.number().required(),
     pricePerDay: joi.number().greater(0).required(),
